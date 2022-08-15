8847e4c38c6ad2a9fa97405241c5386c34d0f4b7
robertacuna22 <rbrtacuna24@gmail.com>
2022-08-15 23:48:17 +0800

    create users model and libraries
    create users model and libraries

---
+++ POS.Inventory/POS.Inventory.Domain/Model/BaseDomain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Inventory.Domain.Model
+{
+    public abstract class BaseDomain
+    {
+        public Guid InstallationId { get; set; }
+    }
+}
